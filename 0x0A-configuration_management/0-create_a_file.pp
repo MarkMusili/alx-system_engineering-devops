c8ff99667c049f46a11186fdb5ed02455e2edb2c
MarkMusili <musiliyrn@gmail.com>
2023-11-24 11:17:39 +0000

    File resource editing/update

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create a file in a named location
+
+file {'/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
