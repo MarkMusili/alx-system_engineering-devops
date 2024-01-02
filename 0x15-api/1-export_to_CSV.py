2177ccaf15d6399da5253ade403533511ef18d5c
MarkMusili <musiliyrn@gmail.com>
2024-01-02 18:10:21 +0300

    Pycodestyle check

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,20 +1,19 @@
 
 #!/usr/bin/python3
-"""Returns Todo list for a given employee id
-and axports data in the CSV format"""
+"""Exports data in the CSV format"""
 import requests
 from sys import argv
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    id = argv[1]
+    user_id = argv[1]
 
-    user = requests.get(url + f"users/{id}").json()
-    todos = requests.get(url + f"todos", params={"userId": id}).json()
+    user = requests.get(url + f"users/{user_id}").json()
+    todos = requests.get(url + f"todos", params={"userId": user_id}).json()
 
-    file = f"{id}.csv"
+    file = f"{user_id}.csv"
     with open(file, "w") as f:
         for item in todos:
             f.write(
-                f'"{id}","{user["username"]}","{item["completed"]}", "{item["title"]}"\n'
+                f'"{user_id}","{user["username"]}","{item["completed"]}", "{item["title"]}"\n'
             )
