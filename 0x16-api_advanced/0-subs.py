c86a744267b1a0af3af289ccbfd456129b6b6931
MarkMusili <musiliyrn@gmail.com>
2024-01-17 10:56:05 +0300

    Query to get subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module to query the Reddit API 
+and returns the number of subscribers 
+or 0 if an invalid subreddit is given."""
+import requests
+
+def number_of_subscribers(subreddit):
+    """Return the number of subscribers"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "My-User-Agent"}
+    r = requests.get(url, headers=headers)
+    if r.status_code == 200:
+        return r.json().get("data")["subscribers"]
+    return 0
