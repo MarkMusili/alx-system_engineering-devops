bf818dd23ff8aa8e76c1818e6ecb1ae4f6371cc6
MarkMusili <musiliyrn@gmail.com>
2024-01-17 22:33:13 +0300

    Top ten titles from hot subreddits

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Print the titles of the first
+10 hot posts listed for a given subreddit."""
+import requests
+
+def top_ten(subreddit):
+    """The function"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "My-User-Agent"}
+    r = requests.get(url, headers=headers)
+    if r.status_code == 200:
+        for i in range(10):
+            print(r.json().get("data")["children"][i]["data"]["title"])
+    else:
+        print("None")
