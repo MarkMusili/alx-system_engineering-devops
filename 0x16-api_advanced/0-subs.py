61463517d0dbf91530d70dcadff019ea359a631c
MarkMusili <musiliyrn@gmail.com>
2024-01-17 23:22:06 +0300

    Dont allow redirects

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     """Return the number of subscribers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "My-User-Agent"}
-    r = requests.get(url, headers=headers)
+    r = requests.get(url, headers=headers, allow_redirects=False)
     if r.status_code == 200:
         return r.json().get("data")["subscribers"]
     return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,7 +8,7 @@ def top_ten(subreddit):
     """The function"""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "My-User-Agent"}
-    r = requests.get(url, headers=headers)
+    r = requests.get(url, headers=headers, allow_redirects=False)
     if r.status_code == 200:
         for i in range(10):
             print(r.json().get("data")["children"][i]["data"]["title"])
