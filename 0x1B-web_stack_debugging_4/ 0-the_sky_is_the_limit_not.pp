3c4c72566cba799e6a6e4fc4a445c600ee25ec88
MarkMusili <musiliyrn@gmail.com>
2024-01-25 19:28:12 +0300

    Update to the puppet

--- 0x1B-web_stack_debugging_4/ 0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/ 0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,11 @@
 # Fix the number of filedescriptor limit
-exec {'Change the ulimit value':
-        command => 'sed -i "s/15/1024/g" /etc/default/nginx; service nginx restart',
-        path    => '/usr/bin:/bin'
+exec { 'Change the ulimit':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
